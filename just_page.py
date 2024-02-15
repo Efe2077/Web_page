fcb070448e936ef3ee1ff7abd68e8eadcf0c004f
Efe <efekochak2017@gmail.com>
2024-02-15 18:42:08 +0300

    Пасхалочка

--- just_page.py
+++ just_page.py
@@ -159,5 +159,27 @@ def results(nickname, level, rating):
                 """.format(nickname, level, rating)
 
 
+@app.route('/cat')
+def cat():
+    url_cat = url_for('static', filename='img/dancing_bold_cat.gif')
+    return """<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Котик</title>
+                      </head>
+                      <body>
+                        <img src="{}" 
+                        alt="здесь должна была быть картинка, но не нашлась">
+                      </body>
+                    </html>
+                    """.format(url_cat)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
