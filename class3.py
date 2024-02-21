f442bd9a4c46249a61f24d8aa99c37d78bb56c82
Efe <efekochak2017@gmail.com>
2024-02-21 12:42:29 +0300

    Список профессий

---
+++ class3.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('base.html', title="Марс")
+
+
+@app.route('/list_prof/<lst>')
+def list_prof(lst):
+    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                   'инженер по терраформированию', 'климатолог',
+                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                   'штурман', 'пилот дронов']
+
+    return render_template('list_prof.html', list=lst.lower(), prof=professions)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
